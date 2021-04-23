9940d1dbdf386f09962f72e7c124cfaa50ed33d9
Antonio Oliveira <antonio_jorge15@hotmail.com>
2021-04-23 00:00:21 -0300

    Implementação de Authorize

--- NcDonalds/Areas/Admin/Controllers/AdminLancheController.cs
+++ NcDonalds/Areas/Admin/Controllers/AdminLancheController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using NcDonalds.Models;
 using NcDonalds.Repositories.Interfaces;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 namespace NcDonalds.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminLancheController : Controller
     {
         private readonly ILancheRepository _lancheRepository;
